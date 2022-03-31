5bc38a01373fd2aaa97a31db3871e0c6d5aaf275
neomagic100 <neomagic100@gmail.com>
2022-03-31 12:56:40 -0400

    Added Item superclass
    Item superclass to call MonoBehavior.
    individual Items will call Item

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    protected string id;
+    protected int size_x;
+    protected int size_y;
+
+    public void Move()
+    {
+
+    }
+
+    public void Remove()
+    {
+
+    }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ()
+    }
+}
