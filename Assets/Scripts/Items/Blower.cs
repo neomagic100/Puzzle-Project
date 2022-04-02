e4487916a66baf194aac6b98e776a7fd891cb4ce
neomagic100 <neomagic100@gmail.com>
2022-04-01 20:00:45 -0400

    Added sound effect to blower
    messed up animation. Either Loops or doesnt move.

--- Assets/Scripts/Items/Blower.cs
+++ Assets/Scripts/Items/Blower.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Blower : Item
 {
+    private AudioSource audioSrc;
+    public Rigidbody2D rb;
+    private Animation blowingAnimation;
+    [SerializeField] private Animator blowingAnimator;
+
     public Blower()
     {
         
     }
 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // If a collision is detected with an Item
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            audioSrc.Play();
+            blowingAnimator.enabled = true;
+            blowingAnimator.StartPlayback();
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        // Load blowing sound fx
+        audioSrc = GetComponent<AudioSource>();
+
+        if (audioSrc == null)
+        {
+            Debug.LogError("Error loading AudioSource for Blower Item");
+        }
+
+        // Load blowing animation
+        blowingAnimator = GetComponent<Animator>();
         
+        if (blowingAnimator == null)
+        {
+            Debug.LogError("Cannot load animation for the Blower item.");
+        }
+
+        blowingAnimator.enabled = false;
+        /* else
+         {
+             // Set animation to not play automatically
+             blowingAnimation.playAutomatically = false;
+             blowingAnimator.SetBool("blowersheet2_1", false);
+         }*/
+
     }
 
     // Update is called once per frame
