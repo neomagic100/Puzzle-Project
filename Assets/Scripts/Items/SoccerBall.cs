e7a69d6bd22afad46bcbdeb79d36794800424032
David Ryder <david.winfield2000@gmail.com>
2022-04-04 15:10:21 -0400

    Basics of ball mechanics

--- Assets/Scripts/Items/SoccerBall.cs
+++ Assets/Scripts/Items/SoccerBall.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BilliardsBall : MonoBehaviour
+public class SoccerBall : MonoBehaviour
 {
-    // Start is called before the first frame update
     private void OnCollisionEnter(Collision obj)
     {
-        if (obj.gameObject.name == "Goal")
+        if (obj.gameObject.tag == "goal")
             print("Success!");
     }
 }
