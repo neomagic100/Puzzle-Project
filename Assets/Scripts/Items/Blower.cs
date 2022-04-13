6829fff5f95805477bbd10a3690a5eaab07a8c2d
neomagic100 <neomagic100@gmail.com>
2022-04-13 15:24:35 -0400

    Update Blower.cs

--- Assets/Scripts/Items/Blower.cs
+++ Assets/Scripts/Items/Blower.cs
@@ -8,22 +8,34 @@ public class Blower : Item
     public Rigidbody2D rb;
     private Animator blowingAnimator;
     private bool audioPlayed;
+    private bool collDetected = false;
+    
   
     // When an item collides with Blower, play sound and animation
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        collDetected = true;
+        gameObject.GetComponent<BoxCollider2D>().usedByEffector = collDetected;
         // If a collision is detected with an Item
         if (collision.gameObject.CompareTag("Item"))
         {
+            // Since it is now blowing, set the trigger zone to true
+            gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+        
             blowingAnimator = GetComponent<Animator>();
            
             if (!audioPlayed)
             {
                 audioSrc.Play();
             }
-            
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameObject.GetComponent<BoxCollider2D>().usedByEffector = collDetected;
+    }
+
 
     // When item leaves Blower, stop animation
     private void OnCollisionExit2D(Collision2D collision)
@@ -33,7 +45,7 @@ public class Blower : Item
 
     // Start is called before the first frame update
     void Start()
-    {
+    {     
         // Load blowing sound fx
         audioSrc = GetComponent<AudioSource>();
 
@@ -56,5 +68,14 @@ public class Blower : Item
             audioPlayed = true;
         }
 
+        Debug.Log(collDetected);
+    }
+
+    private void setupEffector(PointEffector2D pe)
+    {
+        pe.forceSource = EffectorSelection2D.Rigidbody;
+        pe.forceTarget = EffectorSelection2D.Rigidbody;
+        pe.forceMagnitude = 100;
+        pe.forceMode = EffectorForceMode2D.Constant;
     }
 }
