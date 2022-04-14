ee81121a973b6c6ee4fccb2899b659c64c59594c
neomagic100 <neomagic100@gmail.com>
2022-04-14 13:41:51 -0400

    Moved fan script to Item scripts, changed Parent Class to Item

--- Assets/Scripts/Items/fan.cs
+++ Assets/Scripts/Items/fan.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fan : MonoBehaviour
+public class fan : Item
 {
     private Rigidbody2D obj;
     public int force;
