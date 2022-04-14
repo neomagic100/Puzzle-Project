586f9242a881e2370fcae6839dd55dbc170cc96a
David Ryder <david.winfield2000@gmail.com>
2022-04-14 04:10:07 -0400

    created functional fan object

--- Assets/Scripts/fan.cs
+++ Assets/Scripts/fan.cs
@@ -2,19 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FanScript : MonoBehaviour
+public class fan : MonoBehaviour
 {
-    private Rigidbody2D obj; // object that collides with the fan's hitbox
-
+    private Rigidbody2D obj;
     public int force;
-
-    public void onCollisionEnter2D(Collider2D item)
+    private void OnTriggerStay2D (Collider2D item)
     {
         if (item.gameObject.CompareTag("Item"))
         {
             obj = item.GetComponent<Rigidbody2D>();
-
-            obj.AddForce(new Vector2(force, 0));
+            obj.AddForce(new Vector2(obj.velocity.x + force, obj.velocity.y));
         }
     }
 }
