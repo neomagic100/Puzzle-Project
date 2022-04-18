b3f6fc314e24762fddc3c073f9453431cf371c1e
getusername67 <61632919+getusername67@users.noreply.github.com>
2022-04-17 21:54:10 -0400

    Balloon anim fixed

--- Assets/Scripts/Items/Balloon.cs
+++ Assets/Scripts/Items/Balloon.cs
@@ -1,48 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 // Matthew Sanders
 // script for the balloon item. The balloon will pop once it touches the dart which has the tag "sharp"
 
 
-public class Balloon : Item
+public class Balloon : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public GameObject balloon;
-    public Transform SharpCheck;
-    public float SharpCheckRadius;
-    public LayerMask WhatIsSharp;
-    private bool touchSharp;
-    private Animator myBalloon;
-    private AudioSource popSound;
-    private bool audioPlaying;      // this is used so that the popping sound only plays once
-
-    void Start()
+	public Rigidbody2D rb;
+	public GameObject balloon;
+	private Animator myBalloon;
+	private AudioSource popSound;
+	private bool audioPlaying;		// this is used so that the popping sound only plays once
+	
+void Start() {
+     rb = GetComponent<Rigidbody2D>();
+	 myBalloon = GetComponent<Animator>();
+	 popSound = GetComponent<AudioSource>();
+	 audioPlaying = false;
+	}
+	
+	private void OnCollisionEnter2D(Collision2D collision)
     {
-        rb = GetComponent<Rigidbody2D>();
-        myBalloon = GetComponent<Animator>();
-        if (myBalloon == null)
+        // If a collision is detected with an Item
+        if (collision.gameObject.CompareTag("sharp"))
         {
-
-        }
-        popSound = GetComponent<AudioSource>();
-        popSound.volume = AudioManager.instance.volume;
-        audioPlaying = false;
-    }
-
-    void FixedUpdate()
-    {
-        touchSharp = Physics2D.OverlapCircle(SharpCheck.position, SharpCheckRadius, WhatIsSharp);
-        if (touchSharp)
-        {
-            myBalloon.Play("balloonPop");
-            if (audioPlaying == false)
-            {
-                popSound.Play();
-            }
-            audioPlaying = true;
-            Destroy(gameObject, 0.5f);
+			myBalloon.Play("balloonPop");
+			 if(audioPlaying == false){
+				 popSound.Play();
+			 }
+			 audioPlaying = true;
+			 Destroy(gameObject, 0.08f);
         }
     }
-
+	
 
 }
