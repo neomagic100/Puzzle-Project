d9122f3fe0e138faf02b9ce1a9541311d3a0ff95
Joshua Villemain <joshua.villemain@gmail.com>
2022-04-19 13:00:46 -0400

    Scene Change patch

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
+    [Header("Unity References")]
+    public GameObject victoryScreen;
+
     private void OnTriggerEnter2D(Collider2D item)
     {
         if (item.gameObject.name == "SoccerBall(Clone)")
         {
+            // Creates the Victory Screen to traverse to the next level
+            Instantiate(victoryScreen);
+
+            // Saves the Game
+            float volume = AudioManager.instance.volume;
+            float unlocks = SceneManager.GetActiveScene().buildIndex;
+
+            SaveData.SaveGame(unlocks, volume);
             Debug.Log("Success!");
         }
     }
--- Assets/Scripts/VictoryScore.cs
+++ Assets/Scripts/VictoryScore.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class VictoryScore : MonoBehaviour
 {
     [Header("Unity References")]
     public Text score;
+    public GameObject nextLevel;
     private GameObject timerObj;
     private Timer timer;
 
@@ -24,5 +26,36 @@ public class VictoryScore : MonoBehaviour
 
         // Updates the Score
         score.text = timer.score.ToString();
+
+        // Checks if final Level
+        if (SceneManager.GetActiveScene().buildIndex == 3)
+            nextLevel.SetActive(false);
+    }
+
+    // =============================================
+    //                  Buttons
+    // =============================================
+    public void OnMainMenuButton()
+    {
+        // Audio
+        ButtonClick();
+
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnNextLevelButton()
+    {
+        // Audio
+        ButtonClick();
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ButtonClick()
+    {
+        if (AudioManager.instance == null)
+            return;
+
+        AudioManager.instance.Click();
     }
 }
